e2807d7a3322704c50a0017efa947f559a19b42c
Andrew Simmons <agsimmons0@gmail.com>
2023-10-02 18:27:47 -0400

    Combine demo pages

--- demo_app/urls.py
+++ demo_app/urls.py
@@ -4,6 +4,5 @@ from demo_app import views
 
 
 urlpatterns = [
-    path("", views.chat_view),
-    path("user_log/", views.user_log_view),
+    path("", views.demo_view),
 ]
--- demo_app/views.py
+++ demo_app/views.py
@@ -1,9 +1,5 @@
 from django.shortcuts import render
 
 
-def chat_view(request):
-    return render(request, "demo_app/chat.html")
-
-
-def user_log_view(request):
-    return render(request, "demo_app/user_log.html")
+def demo_view(request):
+    return render(request, "demo_app/demo.html")
